72bb1595f19ee3276499c65d43c7a038f3c0ba68
ke4tri <ke4tri@gmail.com>
2021-07-20 20:29:52 -0500

    adding Gallery Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 //import logo from './logo.svg';
+import React from "react";
 import About from './components/About';
-import New from './components/New';
 import Nav from './components/Nav';
+import Gallery from "./components/Gallery";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
-      <header className="App-header">
-      <About></About>
-      <New></New>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <main>
+  <Gallery></Gallery> 
+  <About></About>
+  </main>
+      
     </div>
   );
 }
