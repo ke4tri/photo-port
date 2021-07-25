0ed4eeb7d488dbfdc179625a0b83c19db109335f
ke4tri <ke4tri@gmail.com>
2021-07-25 15:58:59 -0500

    dynamically change discription render of nav selections

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../src/utils/helpers'
+import PhotoList from '../components/PhotoList'
 
-function Gallery(){
-    const currentCategory = {
-        name:"commercial",
-        description:
-            "Photos of grocery stores, food trucks, and other commercial projects"
-    };
-    return(
-        <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-        </section>
+// function Gallery(){
+//     const currentCategory = {
+//         name:"commercial",
+//         description:
+//             "Photos of grocery stores, food trucks, and other commercial projects"
+//     };
+//     return(
+//         <section>
+//             <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+//             <p>{currentCategory.name}</p>
+//             <PhotoList />
+//         </section>
+//     );
+// }
+
+function Gallery({ currentCategory }) {
+    const { name, description } = currentCategory;
+    return (
+      <section>
+        <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+        <p>{description}</p>
+        <PhotoList  />
+      </section>
     );
-}
+  }
 
 export default Gallery;
